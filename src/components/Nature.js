8ae65257d732d68bcc2a52f5279ddf5b355939e7
shaebukhan <97784597+shaebukhan@users.noreply.github.com>
2024-12-15 12:26:46 +0500

    Delete src directory

--- src/components/Nature.js
+++
@@ -1,3 +0,0 @@
-const randomCategories = [
-    "Desktop Computers", "Notebooks", "Components", "Gaming", "Cases&Moding", "Cooling", "Moniters", "Peripherals", "Cable&Accessories", "Network Devices"
-];
